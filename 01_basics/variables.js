21f8d6869cdc95587b7bc2cad4c9841473b68b08
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-04 08:53:21 +0000

    container

---
+++ 01_basics/variables.js
@@ -0,0 +1,14 @@
+const accountId = 23456
+let accountEmail = "amarjeetchoudhary@gmail.com"
+var accountPassword = "86690"
+accountCity = "bengluru"
+
+//accountId = 24321 // not allowed
+
+accountEmail = "amarjeetchoudhary676897"
+accountPassword = "244345"
+accountCity = "bihar"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);

6c6ef9d26115d14bdce2b3020fd4206a37a25c9a
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-17 18:01:24 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+let score = "hitesh"
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+//33 => it is a number value
+//33abc => it i s not number value in simple way it is a nan not an number
+//true => converts into 1, false = value converts into 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true => false
+//"" => false //empty strng always give false value
+//"Amarjeet" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));

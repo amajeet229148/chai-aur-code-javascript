c2f425f70917d0897bbda56cdcac2a046fc2a71b
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-17 18:57:40 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 < 1);
+// console.log(2 <=1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);   // avoids most of the time these conversion
+// console.log("02" > 1);  // avoids most of the time these conversion
+
+// console.log(null > 0);  // avoids most of the time these conversion
+// console.log(null == 0); // avoids most of the time these conversion
+// console.log(null >= 0); // avoids most of the time these conversion
+
+// console.log(undefined == 0); // avoids most of the time these conversion
+// console.log(undefined > 0); // avoids most of the time these conversion
+// console.log(undefined < 0); // avoids most of the time these conversion
+
+// === uses for strict check with your datatypes
+console.log("2" === 2);

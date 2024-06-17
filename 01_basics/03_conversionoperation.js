6d047d1e16fb0a13414a60a516b4d45837297104
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-17 18:37:01 +0000

    conversion

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -21,5 +21,44 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof(stringNumber));
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+//  ***********operation*********** 
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-3);
+// console.log(2*3);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "amar"
+let str2 = " choudhary"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log("1" + "2");
+// console.log("1" + 2 + 2); //javascript uses end numbers as a string
+// console.log(1 + 2 + "2"); //javascript treat starting numbers as a conversion 
+
+// console.log((2 + 2) * 3 % 4); //we have to use peranthesis in big conversion 
+
+console.log(+true); // dont have to use these complex code because redability is important
+console.log(+"");   // dont have to use these complex code because redability is important
+
+let num1, num2, num3 
+num1, num2, num3 = 2 + 2 // dont have to use these complex code because redability is important
+
+let gameCounter = 100
+gameCounter++; // prefix
+++gameCounter;  // postfix
+//console.log(gameCounter);
+
+

5dea351d7fefcd33fe573af9d5ae1d696d6b2f8a
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-17 17:17:32 +0000

    dataypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict" //treat all code aqs a newer version
+
+console.log(3 
+    + 3); // code redability should be high
+
+let name = "amarjeet"
+let age = 29
+let loggedIn = false
+
+//------dataypes
+//number = its range is 2 power 53
+//bigInt = uses for big number for trading stalk marketing and facebook and reddit
+//string = we can use in single '', or in double "" as per choice but we can prefer double""
+//boolean = true or false
+//null = it is stand alone value basically it is a type, basically it is a empty value
+//undefined = when we dont assign value in variable
+//symbol = uses for identify uniqeness - it is a function
+//object = 
+
+console.log(typeof Symbol);

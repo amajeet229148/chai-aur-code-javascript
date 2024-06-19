489dff1d16308489906771a6dc406bb04a57fcbb
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-19 10:26:57 +0000

    date

--- 01_basics/07_date_in.js
+++ 01_basics/07_date_in.js
@@ -1,12 +1,20 @@
 // Date
 
 let myDate = new Date()
-console.log(myDate.toDateString());   // output: tue jun 18 2024
-console.log(myDate.toLocaleDateString());   // output: 6/18/2024
-console.log(myDate.toLocaleTimeString());  // output: 11:41;25 AM
-console.log(myDate.toString());  // output: tue jun 18 2004 11:41:25 GMT+0000 (coordinated universal time)
-console.log(myDate.toTimeString());  // output:11:21:25 GMT=0000 (coordinated universal time)
-console.log(myDate.toUTCString());  // output: tue, 18 jun 2024 11:41:25 GMT
+// console.log(myDate.toDateString());   // output: tue jun 18 2024
+// console.log(myDate.toLocaleDateString());   // output: 6/18/2024
+// console.log(myDate.toLocaleTimeString());  // output: 11:41;25 AM
+// console.log(myDate.toString());  // output: tue jun 18 2004 11:41:25 GMT+0000 (coordinated universal time)
+// console.log(myDate.toTimeString());  // output:11:21:25 GMT=0000 (coordinated universal time)
+// console.log(myDate.toUTCString());  // output: tue, 18 jun 2024 11:41:25 GMT
 
-console.log(typeof myDate); //object
+// console.log(typeof myDate); //object
 
+let myCreatedDate = new Date("2023-01-14")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp =  Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));

9d0472408d1ffd3c31b71796565afbfe173c9fc5
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-23 00:19:46 +0000

    execution

---
+++ 03_basics/05_execution.js
@@ -0,0 +1,10 @@
+//javascript execution context  =>basically it is the way hoe javascript run the codes javascript runs code into two phases
+
+//types of execution context :
+  //global conext and function conext and eval it is a property of global context
+
+// type of execvution phases in javascript :
+  // 1. memory creation phase and,  2.execute phase
+
+  1. //memory creation phase used to allocation of memory of variable
+  2. //used to excecute the operation

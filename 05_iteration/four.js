1e5476e53ff48965248b368696efa37f6bbe5c89
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-06-24 12:57:31 +0000

    four

--- 05_iteration/four.js
+++ 05_iteration/four.js
@@ -6,6 +6,29 @@ const myObject = {
 }
 
 for (const i in myObject) {
-    console.log(`${i} shortcut of ${myObject[i]}`);
+    // console.log(`${i} shortcut of ${myObject[i]}`);
         
     }
+     
+
+    const arr = ["js", "ruby", "cpp", "python"]
+    for (const key in arr) {
+        console.log(arr[key]);
+        }
+        
+
+
+
+        ////not iterateable
+        const map = new Map()
+
+     map.set('IN', "india")
+     map.set('USA', "united states of india")
+     map.set('FR', "france")
+     map.set('IN', "india")
+
+     for (const key in map) {
+       console.log(key[map]);
+            
+        }
+     

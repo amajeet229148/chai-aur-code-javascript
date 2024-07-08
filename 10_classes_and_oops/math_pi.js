380d6f923fe9b472b28c3a84d68ef56bd61303d8
amajeet229148 <135828748+amajeet229148@users.noreply.github.com>
2024-07-08 03:49:49 +0000

    math

--- 10_classes_and_oops/math_pi.js
+++ 10_classes_and_oops/math_pi.js
@@ -1,46 +1,28 @@
-const descriptor = Object.getOwnPropertyDescriptor(Math, "PI")
-// Math.PI = 5
-// console.log(Math.PI);
+const descriptor = Object.getOwnPropertyDescriptor(Math, 'PI')
+Math.PI = 5
 
 console.log(descriptor);
 
-
-const chai = {  
-  name: "amarjeet",
-  loggedin: true,
-  email: "amarjeetchoudhary647@gmail.com"
+const chai = {
+  name: 'chai',
+  price: 34,
+  isAvailable: true,
+  
+  myobj:function(){
+    console.log(`thats ok`);
+  }
 }
 
-console.log(Object.getOwnPropertyDescriptor(chai, "email"));
 
-
-// Object.defineProperty(Math, 'PI', {
-//     writable: true,
-//     enumerable: true,
-//     configurable: true
-// })
-// console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
-// 
-
-// Object.defineProperty(math, "Math.pi", {
-//     writable: true,
-//     enumerable: true,
-//     configurable: true
-// })
-// console.log(Object.getOwnPropertyDescriptor(math, "Math.PI"));
-
-
-// ++++++++ this is how to change math.pi property
-// const math = {
-//     name: "manish",
-//     class: Math.PI,
-// }
-
-
-const math = Math.PI = 5
-Object.defineProperty(Math, 'PI', {
-    enumerable: true, 
-    writable: true,
-    configurable: true
+Object.defineProperty(chai, 'price', {
+  writable: false,
+  enumerable: true
 })
-console.log(Object.getOwnPropertyDescriptor(Math, 'PI'));
+console.log(Object.getOwnPropertyDescriptor(chai, 'price'));
+
+for (const [key, value] of Object.entries(chai)) {
+  if (typeof value !== 'function') {
+    console.log(`${key}, ${value}`);
+  }
+    
+}
--- 10_classes_and_oops/object.js
+++ 10_classes_and_oops/object.js
@@ -40,3 +40,25 @@ The constructor is called: The constructor function is called with the specified
 The new object is returned: After the constructor function has been called, if it doesn't return a non-primitive value (object, array, function, etc.), the newly created object is returned.
 
 */
+
+function flatten(array, result) {
+    if (array.length === 0) {
+      return result
+    }
+    var head = array[0]
+    var rest = array.slice(1)
+    if (Array.isArray(head)) {
+      return flatten(head.concat(rest), result)
+    }
+    result.push(head)
+    return flatten(rest, result)
+  }
+  
+  console.log(flatten([], []))
+  console.log(flatten([1], []))
+  console.log(flatten([1,2,3], []))
+  console.log(flatten([1,2,[3,4]], []))
+  console.log(flatten([1,2,[3,[4,5,6]]], []))
+  console.log(flatten([[1,2,3],[4,5,6]], []))
+  console.log(flatten([[1,2,3],[[4,5],6,7]], []))
+  console.log(flatten([[1,2,3],[[4,5],6,[7,8,9]]], []))
